f089d476ed3f66db8023cae2a1d47d22f19a160c
vctrthe <victorthe123@gmail.com>
2026-09-22 18:25:30 +0700

    chapt21: conditionals - switch statement and expression

--- MyTool/main.swift
+++ MyTool/main.swift
@@ -47,3 +47,40 @@ let isValidSleep = hoursSlept >= 0
 
 let someValue = hoursSlept > 17 ? "Amazing" : "Oh no!"
 let anotherValue: Double = hoursSlept > 0 ? 0 : 7.3
+
+print("\n\nSwitch Statement and Expression")
+
+let dayOfWeek = "Wednesday"
+switch dayOfWeek {
+case "Monday":
+    print("It's the start of the work week")
+case "Tuesday":
+    print("It's still early in the week")
+case "Wednesday":
+    print("The week is halfway through")
+case "Thursday":
+    print("The weekend is approaching 😀")
+case "Friday":
+    print("It's the end of the work week")
+case "Saturday", "Sunday":
+    print("Yes! It is the weekend")
+default:
+    print(#""\#(dayOfWeek)" is invalid day of the week!"#)
+}
+
+let message: String = switch dayOfWeek {
+case "Monday":
+    "It's the start of the work week"
+case "Tuesday":
+    "It's still early in the week"
+case "Wednesday":
+    "The week is halfway through"
+case "Thursday":
+    "The weekend is approaching 😀"
+case "Friday":
+    "It's the end of the work week"
+case "Saturday", "Sunday":
+    "Yes! It is the weekend"
+default:
+    #""\#(dayOfWeek)" is invalid day of the week!"#
+}
